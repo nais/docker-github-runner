dd470413a1d917f82e2fbb527e9671df1ee25a32
Terje Sannum <terje.sannum@nav.no>
2020-11-04 14:42:51 +0100

    reconfigure on restart

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
-if test -z "${RUNNER_TOKEN}"; then
+while true; do
     payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token)
     RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
-fi
-
-/opt/runner/config.sh \
-    --name ${HOSTNAME} \
-    --labels platform-apps-runner,${NAIS_CLUSTER_NAME} \
-    --token ${RUNNER_TOKEN} \
-    --url https://github.com/${GITHUB_REPO} \
-    --work $HOME/_work \
-    --unattended \
-    --replace
-
-while true; do
+    /opt/runner/config.sh \
+        --name ${HOSTNAME} \
+        --labels platform-apps-runner,${NAIS_CLUSTER_NAME} \
+        --token ${RUNNER_TOKEN} \
+        --url https://github.com/${GITHUB_REPO} \
+        --work $HOME/_work \
+        --unattended \
+        --replace
     /opt/runner/run.sh
-    echo Runner exit: unregistering
-    /opt/unregister-runner.sh
     echo Restarting in 1m...
     sleep 60
 done
--- unregister-runner.sh
+++ unregister-runner.sh
@@ -2,9 +2,7 @@
 
 echo Unregister runner...
 
-if test -z "${RUNNER_TOKEN}"; then
-    payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token)
-    RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
-fi
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token)
+RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
 /opt/runner/config.sh remove --unattended --token "${RUNNER_TOKEN}"
