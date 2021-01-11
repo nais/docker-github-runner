fa65a8420e2006ed20592c655a1488b7fcfd7ed3
Terje Sannum <terje.sannum@nav.no>
2021-01-11 09:23:05 +0100

    removed loop

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-while true; do
-    payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token)
-    RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
-    /opt/runner/config.sh \
-        --name ${HOSTNAME} \
-        --labels platform-apps-runner,${NAIS_CLUSTER_NAME} \
-        --token ${RUNNER_TOKEN} \
-        --url https://github.com/${GITHUB_REPO} \
-        --work $HOME/_work \
-        --unattended \
-        --replace
-    /opt/runner/run.sh
-    /opt/runner/config.sh remove --unattended --token "${RUNNER_TOKEN}"
-    echo Restarting in 1m...
-    sleep 60
-done
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token)
+RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
+/opt/runner/config.sh \
+    --name ${HOSTNAME} \
+    --labels platform-apps-runner,${NAIS_CLUSTER_NAME} \
+    --token ${RUNNER_TOKEN} \
+    --url https://github.com/${GITHUB_REPO} \
+    --work $HOME/_work \
+    --unattended \
+    --replace
+/opt/runner/bin/runsvc.sh
