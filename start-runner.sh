c54de88b74247eba9d85888e759d6c30aa7535a5
Terje Sannum <terje.sannum@nav.no>
2022-02-02 15:20:55 +0100

    add option to delete existing runners at startup

--- start-runner.sh
+++ start-runner.sh
@@ -1,4 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# Setting RUNNER_UNIQ_LABEL will delete other runners with this label.
+# This can be used to remove registered runners that are offline due to e.g. an unclean shutdown
+if test -n "$RUNNER_UNIQ_LABEL"; then
+    for runner in $(curl -sH "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners | jq -r ".runners | .[]? | select(.labels[].name == \"${RUNNER_UNIQ_LABEL}\").id"); do
+        echo "Deleting existing runner with label $RUNNER_UNIQ_LABEL: $runner"
+        curl -sX DELETE -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/$runner
+    done
+fi
 
 payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${GITHUB_REPO}/actions/runners/registration-token)
 RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
